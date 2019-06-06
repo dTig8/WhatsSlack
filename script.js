433f73274e72a445b8f8c59a00325ae20230128c
ppohlitze <paul.pohlitz@gmx.de>
2019-06-06 09:03:51 +0200

    lots of progress

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 $(document).ready(function () {
     $("form").submit(function (event) {
-        $.ajax({
-            url: "add url here",
-            headers: {
-                "X-Group-Token": "add token here",
-            },
-            method: "POST",
-            contentType: "application/json",
-            data: formToJsonString($(" form ")),
-            success: function () {
-                alert("Creating the new Channel was successful!");
-            },
-            error: function () {
-                alert("Failure! This channel name is already taken!");
-            }
-        });
+        
+        makePOSTrequest("", $(" form "), formToJsonString);
+
         event.preventDefault();
     });
 
@@ -23,45 +11,27 @@ $(document).ready(function () {
     createUsername();
 });
 
-window.onload = function () {
-    $.ajax({
-        url: "add url here",
-        headers: {
-            "X-Group-Token": "add token here",
-        },
-        method: "GET",
-        dataType: "json",
-    }).done(function (data) {
-        $("#results").empty();
-        $.each(data._embedded.channelList, function (key, value) {
-            $("#results").append(JSON.stringify(value.name) + "<br>");
-        });
+window.onload = async function () {
+    let data = await makeGETrequest("");
+    $("#results").empty();
+    $.each(data._embedded.channelList, function (key, value) {
+        $("#results").append(JSON.stringify(value.name) + "<br>");
     });
 }
 
-function createUsername() {
+async function createUsername() {
     $("#smbtUser, .popup-overlay").on("click", function () {
         var username = $("#user").val();
         if (username == "") {
             alert("Please enter a username!");
         }
-        $.ajax({
-            url: "add url here",
-            headers: {
-                "X-Group-Token": "add token here",
-            },
-            method: "POST",
-            contentType: "application/json",
-            data: userCreateToJsonString(username),
-            success: function () {
-                alert("Joining the channel was successful");
-            },
-            error: function () {
-                alert("Failure! Somehow couldn't join channel!");
-            }
-        });
-
+        makePOSTrequest("", username, userCreateToJsonString);
+        
         hideCreateUsernamePopup();
+
+        setInterval(loadMessages, 1000);
+
+        showTextField();
     });
 }
 
@@ -81,4 +51,40 @@ function userCreateToJsonString(username) {
 
 function formToJsonString(form) {
     return JSON.stringify({ "name": $(form).find('[name="name"]').val(), "topic": $(form).find('[name="topic"]').val() });
+}
+
+async function loadMessages() {
+    let data = await makeGETrequest("");
+    $(".messages").empty();
+    console.log("Feuer frei!");
+    $.each(data._embedded.messageList, function (key, value) {
+        $(".messages").append(JSON.stringify(value.content) + "<br>");
+    });
+}
+
+function showTextField() {
+    $(".message-box").addClass("active");
+}
+
+function makeGETrequest(url) {
+    return $.ajax({
+        url: url,
+        headers: {
+            "X-Group-Token": "",
+        },
+        method: "GET",
+        dataType: "json",
+    });
+}
+
+function makePOSTrequest(url, data, funct) {
+    $.ajax({
+        url: url,
+        headers: {
+            "X-Group-Token": "",
+        },
+        method: "POST",
+        contentType: "application/json",
+        data: funct(data)
+    });
 }
