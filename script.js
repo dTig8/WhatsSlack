db4ca65e2af4ed619627081c2c4bad9c0490a9b8
ppohlitze <paul.pohlitz@gmx.de>
2019-06-10 09:02:24 +0200

    sending messages works; joining different channels works; credentials moved to config file

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+var selectedChannel;
+
 $(document).ready(function () {
     $("form").submit(function (event) {
         
-        makePOSTrequest("", $(" form "), formToJsonString);
+        makePOSTrequest("http://34.243.3.31:8080/channels", $(" form "), formToJsonString);
 
         event.preventDefault();
     });
@@ -12,49 +14,43 @@ $(document).ready(function () {
 });
 
 window.onload = async function () {
-    let data = await makeGETrequest("");
-    $("#results").empty();
+    let data = await makeGETrequest("http://34.243.3.31:8080/channels");
+    $("#channelList").empty();
     $.each(data._embedded.channelList, function (key, value) {
-        $("#results").append(JSON.stringify(value.name) + "<br>");
+        $("#channelList").append($("<li>").attr("id", value.id).attr("class", ".").text(value.name));
     });
 }
 
-async function createUsername() {
+function createUsername() {
     $("#smbtUser, .popup-overlay").on("click", function () {
         var username = $("#user").val();
         if (username == "") {
             alert("Please enter a username!");
         }
-        makePOSTrequest("", username, userCreateToJsonString);
+        makePOSTrequest("http://34.243.3.31:8080/channels/"+ selectedChannel +"/messages", username, userCreateToJsonString);
         
         hideCreateUsernamePopup();
 
-        setInterval(loadMessages, 1000);
-
-        showTextField();
+        facilitateMessageBox();
     });
 }
 
-function hideCreateUsernamePopup() {
-    $(".popup-overlay, .popup-content").removeClass("active");
+function facilitateMessageBox() {
+    setInterval(loadMessages, 1000);
+
+    showTextField();
+
+    sendMessage();
 }
 
-function showCreateUsernamePopup() {
-    $("#results").click(function () {
-        $(".popup-overlay, .popup-content").addClass("active");
-    });
-}
-
-function userCreateToJsonString(username) {
-    return JSON.stringify({ "creator": username, "content": username +" has joined the channel." });
-}
-
-function formToJsonString(form) {
-    return JSON.stringify({ "name": $(form).find('[name="name"]').val(), "topic": $(form).find('[name="topic"]').val() });
+function sendMessage() {
+    $("#send").click(function () {
+        makePOSTrequest("http://34.243.3.31:8080/channels/"+ selectedChannel +"/messages", $("#messageInput"), messageFormToJsonString);
+    })
 }
 
 async function loadMessages() {
-    let data = await makeGETrequest("");
+    let data = await makeGETrequest("http://34.243.3.31:8080/channels/"+ selectedChannel +"/messages");
     $(".messages").empty();
     console.log("Feuer frei!");
     $.each(data._embedded.messageList, function (key, value) {
@@ -62,15 +58,11 @@ async function loadMessages() {
     });
 }
 
-function showTextField() {
-    $(".message-box").addClass("active");
-}
-
 function makeGETrequest(url) {
     return $.ajax({
         url: url,
         headers: {
-            "X-Group-Token": "",
+            "X-Group-Token": accessToken,
         },
         method: "GET",
         dataType: "json",
@@ -81,10 +73,38 @@ function makePOSTrequest(url, data, funct) {
     $.ajax({
         url: url,
         headers: {
-            "X-Group-Token": "",
+            "X-Group-Token": accessToken,
         },
         method: "POST",
         contentType: "application/json",
         data: funct(data)
     });
+}
+
+function hideCreateUsernamePopup() {
+    $(".popup-overlay, .popup-content").removeClass("active");
+}
+
+function showCreateUsernamePopup() {
+    $(".channelList").on("click", "li", function () {
+        selectedChannel = $(this).attr("id");
+        $(".popup-overlay, .popup-content").addClass("active");
+    });
+}
+
+function showTextField() {
+    $(".message-box").addClass("active");
+}
+
+function userCreateToJsonString(username) {
+    return JSON.stringify({ "creator": username, "content": username +" has joined the channel." });
+}
+
+function formToJsonString(form) {
+    return JSON.stringify({ "name": $(form).find('[name="name"]').val(), "topic": $(form).find('[name="topic"]').val() });
+}
+
+function messageFormToJsonString(textarea) {
+    console.log(textarea.val());
+    return JSON.stringify({ "creator": "test4", "content": textarea.val() });
 }
